bc9ba4d337982d0132f8d7a439ebeb1d25f711bb
Camilo BRAVO <61042366+CamiloBrvo@users.noreply.github.com>
2022-03-28 23:51:33 +0200

    game logic

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
  
     draw(){
@@ -126,6 +127,34 @@ function rectangulatCollision({rectangle1, rectangle2}){
     )
 }
 
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }else if(player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 1 Win'
+    }else if(player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 2 Win'
+    }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer(){
+    if(timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer --
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if(timer === 0){
+        determineWinner({player, enemy, timerId})
+    }
+}
+
+decreaseTimer()
+
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -153,14 +182,22 @@ function animate(){
     //detect for collision
     if(rectangulatCollision({rectangle1: player, rectangle2: enemy})
         && player.isAttacking){
-        console.log('player attack')
         player.isAttacking = false
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
     if(rectangulatCollision({rectangle1: enemy, rectangle2: player})
         && enemy.isAttacking){
-        console.log('enemy attack')
+        player.health -= 20
         enemy.isAttacking = false
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
+
+    //end game based on health 
+    if(enemy.health <= 0 || player.health <= 0){
+        determineWinner({player, enemy, timerId})
+    }
+
 }
 
 animate()
@@ -195,7 +232,6 @@ window.addEventListener('keydown', (event) => {
             break
         case 'ArrowDown':
             enemy.attack()
-            enemy.isAttacking = true
             break
     }
 })
